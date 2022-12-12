9cda65a27f8b190ef8ac5141c47889bae06298a5
nafisahmed42018 <nafisahmed@iut-dhaka.edu>
2022-12-12 11:22:32 +0600

    Nav bar and hero section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'comf' : ['Comfortaa', 'sans-serif'],
+        'lora' : ['Lora', 'serif'],
+        'nunito' : ['Nunito', 'sans-serif'],
+        'libre' : ['Libre Baskerville', 'serif'],
+        'monte' : ['Montserrat', 'sans-serif'],
+        sans: ['Nunito', ... defaultTheme.fontFamily.sans],
+      }
+    },
   },
   plugins: [],
 }
